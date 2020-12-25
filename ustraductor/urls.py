f6f9e66756de3f8c077e028c08d01d35f112e1c0
Maximedu13 <maxime.j.pro@free.fr>
2020-12-25 17:37:02 +0100

    updated requirements6

--- ustraductor/urls.py
+++ ustraductor/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 from main import views
@@ -22,3 +23,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls', namespace='main')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+        url(r'^admin/', admin.site.urls)
+    ] + urlpatterns
