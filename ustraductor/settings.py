c27d18bb5b70de046f2de255ec8fd9d1d270a60e
Maximedu13 <maxime.j.pro@free.fr>
2020-12-25 18:29:10 +0100

    updated requirements4

--- ustraductor/settings.py
+++ ustraductor/settings.py
@@ -125,9 +125,6 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'ustraductor/staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
