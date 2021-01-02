9cee2ab438a325f05ccee56c5397a53c3f47f971
Maximedu13 <maxime.j.pro@free.fr>
2021-01-02 12:34:13 +0100

    changed ADMIN

--- ustraductor/urls.py
+++ ustraductor/urls.py
@@ -23,7 +23,6 @@ app_name = 'ustraductor'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('admin/', admin.site.urls),
     path('', include('main.urls', namespace='main')),
 ]
 
